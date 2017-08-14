d358485f254f28d120f16d4bb4b3c786eb68b114
benjamin bressler and daniel garcia <benjamin+daniel@>
2017-08-14 16:32:08 -0700

    add html,css, js files

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,23 @@
     $(document).ready(function() {
     $("#favorite").submit(function(event) {
     event.preventDefault();
-    $("#favorite").hide(800);
-    $("#results").show(800);
-    var item1 = $("input#item1").val();
-    var item2 = $("input#item2").val();
-    var item3 = $("input#item3").val();
-    var item4 = $("input#item4").val();
-    var userInput = [item1, item2, item3, item4];
-    var array = [item1, item2, item3, item4 ];
-    array.sort();
-    var uppers = array.map(function(x) { return x.toUpperCase(); });
-    alert("My Groceries are: " + uppers);
-    userInput.map(function(item) {
-    });
-  });
+
+    var item1 = $("input#item1").val().split(" ").reverse();
+    // var splitSentence = item1.split (" ");
+    // var mappedSentence = splitSentence.map(function(item1) {
+    //   if (item1.length >= 3) {
+    //     return item1;
+    //   } else {
+    //     false;
+    //   }
+
+    item1.forEach(function(word){
+      if (word.length >= 3) {
+        $("#sentence").append(word + " ");
+      }
+    })
+
+      });
+    //alert(mappedSentence.reverse());
+  //});
 });
